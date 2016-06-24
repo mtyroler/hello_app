f70aef10a4fd6d74116ac89fb5b9a2250fa74a77
mtyroler <mtyroler@gmail.com>
2016-06-23 23:18:49 -0400

    Change routes to #goodbye

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def hello
-		render text: "hola,mundo!"
+		render text: "hola, mundo!"
 	end
+	def goodbye
+		render text: "goodbye, world!"
 end
